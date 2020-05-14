486d4814d7947bb8b0b267a155e32d69dfd1f7db
jusola <juulain@gmail.com>
2020-05-14 18:23:07 +0300

    Switch to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import express from 'express';
-import NodeCache from 'node-cache';
-import slonikQCache from 'slonik-interceptor-query-cache';
-import Slonik from 'slonik';
-const sql = Slonik.sql;
+import mongodb from 'mongodb';
 import bodyParser from 'body-parser';
 import JWT from 'jsonwebtoken';
 import expressJWT from 'express-jwt';
@@ -10,8 +7,10 @@ import crypto from 'crypto';
 import argon2 from 'argon2';
 import uuid from 'uuid';
 import srp from 'secure-remote-password/server.js';
-
+import sanitize from 'mongo-sanitize'
 import Payments from './payments.js'
+
+const MongoClient = mongodb.MongoClient;
 
 //Config
 var fallbackSecret = null
@@ -21,17 +20,18 @@ if(!process.env.secret){
     fallbackSecret = crypto.randomBytes(128).toString('base64')
 }
 
+console.log(process.env)
+
 const secret = process.env.secret || fallbackSecret
 const STRIPE_KEY = process.env.STRIPE_KEY
-const DB_USER = process.env.POSTGRES_USER || 'postgres'
-const DB_PASS = process.env.POSTGRES_PASSWORD || 'postgres'
-const DB_NAME = process.env.POSTGRES_DB || 'postgres'
-const DB_HOST = process.env.POSTGRES_SERVICE_HOST || 'localhost'
-const DB_PORT = process.env.POSTGRES_SERVICE_PORT || 5432
-const DB_URL = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const DB_USER = process.env.MONGO_USERNAME || 'ticked'
+const DB_PASS = process.env.MONGO_PASSWORD || '1234'
+const DB_NAME = process.env.MONGO_DATABASE || 'postgres'
+const DB_HOST = process.env.MONGO_SERVICE_HOST || 'localhost'
+const DB_PORT = process.env.MONGO_SERVICE_PORT || 27017
+const DB_URL = process.env.DATABASE_URL || `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 const PORT = process.env.PORT || 5000;
 const isDev = true;
-const allowedDBrows = ['alarm', 'description', 'name', 'listid'];
 
 const payments = new Payments(STRIPE_KEY)
 
@@ -52,6 +52,10 @@ app.use((req, res, next) => {
 //Bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next)=>{
+    req.body = sanitize(req.body);
+    next();
+})
 
 //JWT middleware
 const JWTmw = expressJWT({
@@ -59,36 +63,12 @@ const JWTmw = expressJWT({
 });
 
 //Setting up DB
-const cache = new NodeCache({
-    checkperiod: 60,
-    stdTTL: 3600, //1 hour
-    useClones: false,
-});
-  
-const hashQuery = (query) => {
-    return JSON.stringify(query);
-};
-
-//Create cacheinterceptor
-const interceptors = [
-    slonikQCache.createQueryCacheInterceptor({
-        storage: {
-        get: (query) => {
-          return cache.get(hashQuery(query)) || null;
-        },
-        set: (query, cacheAttributes, queryResult) => {
-          cache.set(hashQuery(query), queryResult, cacheAttributes.ttl);
-        },
-      },
-    }),
-];
-
-const pool = Slonik.createPool(DB_URL, {
-    interceptors: interceptors,
-    maximumPoolSize: 10,
-    idleTimeout: 30000,
-    connectionTimeout: 15000,
-});
+var db
+MongoClient.connect(DB_URL).then((returnDB)=>{
+    db=returnDB
+}).catch((err)=>{
+    console.error(err)
+})
 
 //Sample response
 app.get('/', (req, res)=>{
@@ -100,19 +80,10 @@ app.get('/', (req, res)=>{
 app.post('/login/salt', async (req, res)=>{ 
     const {clientEphemeralPublic, username} = req.body;
     try {
-        const pgReturn = await pool.maybeOne(sql`
-        -- @cache-ttl 600
-        SELECT 
-            verifier,
-            salt, 
-            userid
-        FROM 
-            users 
-        WHERE 
-            username = ${username}`
-        );
-        if(pgReturn){
-            const { verifier, salt, userid } = pgReturn
+        const user = await db.collection("users").findOne({username: username})
+        console.log(user)
+        if(user){
+            const { verifier, salt, userid } = user
             const serverEphemeral = await srp.generateEphemeral(verifier)
             currentLogins.set(username, {clientEphemeralPublic, serverEphemeralSecret: serverEphemeral.secret, salt, verifier, userid})
             res.json({
@@ -221,51 +192,29 @@ app.post('/login', async(req, res)=>{
 //Register
 app.post('/register', async(req, res)=>{
     const {username, salt, verifier} = req.body;
-    const userid = uuid.v4();
     try{
-        const pgReturn = await pool.maybeOne(sql`
-            -- @cache-ttl 600
-            SELECT
-                userid
-            FROM 
-                users 
-            WHERE 
-                username = ${username}`
-        ); //Check if user exists
-        if(!pgReturn){//User doesnt exist
-            await pool.query(sql`
-                -- @cache-ttl 600
-                    INSERT INTO 
-                    users (userid, username, verifier, salt)
-                VALUES (${userid}, ${username}, ${verifier}, ${salt})
-            `); //Insert
-            let token = JWT.sign({ userid: userid, username: username }, secret, { expiresIn: 129600 }); // Sign JWT token
-            res.json({
-                success: true,
-                err: null,
-                token: token,
-            });
-            await initUser({
-                userid: userid, 
-                username: username
-            })
-            if (isDev) console.log(`User registered ${username}: ${token}`);
-        }else{
-            if (process.env.NODE_ENV == 'development') console.log(`User already exists`);
+        const token = await register(username, salt, verifier)
+        res.json({
+            token: token,
+            err: null,
+            success: true
+        })
+    }
+    catch(err){//Something broke
+        if(typeof err === String){
             res.json({
                 success: false,
                 token: null,
-                err: 'error.register.usernameexists'
+                err: err
+            });
+        }else{
+            console.error(err);
+            res.json({
+                success: false,
+                token: null,
+                err: 'error.servererror'
             });
         }
-    }
-    catch(err){//Something broke
-        console.error(err);
-        res.json({
-            success: false,
-            token: null,
-            err: 'error.servererror'
-        });
     }
 });
 
@@ -394,6 +343,30 @@ async function initUser(user) {
     } catch (error) {
         console.error(error)
         throw 'error.servererror'
+    }
+}
+
+async function register(username, salt, verifier){
+    const user = await db.collection("users").findOne({username: username})
+    console.log(user)
+    if(!user){//User doesnt exist
+        const userid = uuid.v4()
+        await db.collection("users").insert({
+            userid,
+            username,
+            verifier,
+            salt
+        })
+        let token = JWT.sign({ userid: userid, username: username }, secret, { expiresIn: 129600 }); // Sign JWT token
+        initUser({
+            userid: userid, 
+            username: username
+        })
+        if (isDev) console.log(`User registered ${username}: ${token}`);
+        return token
+    }else{
+        if (process.env.NODE_ENV == 'development') console.log(`User already exists`);
+        throw 'error.register.usernameexists'
     }
 }
 
